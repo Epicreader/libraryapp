3ac1d746345028eef62c4af60f7359fcceb71a19
Epicreader <61849658+Epicreader@users.noreply.github.com>
2021-03-20 10:53:51 -0700

    Create mock-data.js

---
+++ src/mock-data.js
@@ -0,0 +1,106 @@
+let mock = [{
+    id: 1,
+    name: "FableHaven",
+    author: "Brandon Mull",
+    comment: []
+    
+  },
+  {
+    id: 2,
+    name: "Fablehaven: Rise of the Evening Star",
+    author: "Brandon Mull",
+    comment: []
+    
+  },
+  {
+    id: 3,
+    name: "Fablehaven: Grip of the Shadow Plague",
+    author: "Brandon Mull",
+    comment: [],
+    
+  },
+  {
+    id: 4,
+    name: "Fablehaven: Secrets of the Dragon Sanctuary",
+    author: "Brandon Mull",
+    comment: [],
+    
+  },
+  {
+    id: 5,
+    name: "Fablehaven: Keys to the Demon Prison",
+    author: "Brandon Mull",
+    comment: [],
+   
+
+  },
+  {
+    id: 6,
+    name: "Artemis Fowl",
+    author: "Eoin Colfer",
+    comment: [],
+  
+  },
+  {
+    id: 7,
+    name: "Artemis Fowl and the Arctic Incident",
+    author: "Eoin Colfer",
+    comment: [],
+   
+  },
+  {
+    id: 8,
+    name: "Artemis Fowl and the Eternity Code",
+    author: "Eoin Colfer",
+    comment: [],
+    
+  },
+  {
+    id: 9,
+    name: "Artemis Fowl and the Opal Deception",
+    author: "Eoin Colfer",
+    comment: [],
+    
+
+  },
+  {
+    id: 10,
+    name: "Artemis Fowl and the Lost Colony",
+    author: "Eoin Colfer",
+    comment: [],
+    
+  },
+  {
+    id: 11,
+    name: "Artemis Fowl and the Time Paradox",
+    author: "Eoin Colfer",
+    comment: [],
+    
+  },
+  {
+    id: 12,
+    name: "Artemis Fowl and the Atlantis Complex",
+    author: "Eoin Colfer",
+    comment: [],
+    
+  },
+  {
+    id: 13,
+    name: "Artemis Fowl and the Last Guardian",
+    author: "Eoin Colfer",
+    comment: [],
+  },
+  {
+    id: 14,
+    name: "The Ruins of Gorlan",
+    author: "John Flanagan",
+    comment: [],
+  },
+  {
+    id: 15,
+    name: "The Burning Bridge",
+    author: "John Flanagan",
+    comment: [],
+  }]
+
+export default mock;
