2fd8e8351093572f9528978e3b017a7c8e502f45
Epicreader <61849658+Epicreader@users.noreply.github.com>
2021-03-20 10:57:02 -0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <Catalog :books="books" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Catalog from '../components/Catalog.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Catalog
+  },
+ computed: {
+  books() {
+    return this.$root.$data.books;
   }
+}
 }
 </script>
